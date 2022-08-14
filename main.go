98d8d3082e87b4f31e471e044efd846e04408c99
pueblomo <77644926+pueblomo@users.noreply.github.com>
2022-08-14 12:21:09 +0200

    delete plant

--- main.go
+++ main.go
@@ -17,23 +17,24 @@ import (
 )
 
 type Plant struct {
-  ID int `json:"id"`
-  Alias  string `json:"alias"`
-  Name string `json:"name"`
-  Infos string `json:"infos"`
-  LightBadge string `json:"lightBadge"`
-  LightDescription string `json:"lightDescription"`
-  WaterBadge string `json:"waterBadge"`
-  WaterDescription string `json:"waterDescription"`
-  MoistBadge string `json:"moistBadge"`
-  MoistDescription string `json:"moistDescription"`
-  ImageName string `json:"imageName"`
-  UserName string `json:"userName"`
-  Date time.Time `json:"date"`
+	ID               int       `json:"id"`
+	Alias            string    `json:"alias"`
+	Name             string    `json:"name"`
+	Infos            string    `json:"infos"`
+	LightBadge       string    `json:"lightBadge"`
+	LightDescription string    `json:"lightDescription"`
+	WaterBadge       string    `json:"waterBadge"`
+	WaterDescription string    `json:"waterDescription"`
+	MoistBadge       string    `json:"moistBadge"`
+	MoistDescription string    `json:"moistDescription"`
+	ImageName        string    `json:"imageName"`
+	UserName         string    `json:"userName"`
+	Date             time.Time `json:"date"`
 }
+
 var db *gorm.DB
 
-func main(){
+func main() {
 	connectToDatabase()
 	app := fiber.New()
 	app.Use(cors.New())
@@ -42,71 +43,75 @@ func main(){
 	log.Fatalln(app.Listen(":8080"))
 }
 
-func connectToDatabase(){
+func connectToDatabase() {
 	viper.SetConfigFile(".env")
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Println(err)
 	}
 	viper.AutomaticEnv()
-	
+
 	pw := viper.GetString("DBPASSWORD")
 	url := viper.GetString("DBURL")
-	dsn := fmt.Sprintf("host=%s user=postgres password=%s port=5432 sslmode=disable",url,pw)
+	dsn := fmt.Sprintf("host=%s user=postgres password=%s port=5432 sslmode=disable", url, pw)
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err!=nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 	db.AutoMigrate(&Plant{})
 }
 
-func setRoutes(app *fiber.App){
-	app.Static("/images","./images")
+func setRoutes(app *fiber.App) {
+	app.Static("/images", "./images")
 
 	plantGroup := app.Group("/plant")
-	plantGroup.Get("/", func (c *fiber.Ctx) error {
+	plantGroup.Get("/", func(c *fiber.Ctx) error {
 		return getPlants(c)
 	})
 
-	plantGroup.Post("/", func (c *fiber.Ctx) error {
+	plantGroup.Post("/", func(c *fiber.Ctx) error {
 		return postPlants(c)
 	})
 
-	plantGroup.Put("/", func (c *fiber.Ctx)  error {
+	plantGroup.Put("/", func(c *fiber.Ctx) error {
 		return putPlants(c)
+	})
+
+	plantGroup.Delete("/", func(c *fiber.Ctx) error {
+		return deletePlant(c)
 	})
 }
 
-func getPlants(ctx *fiber.Ctx) error{
+func getPlants(ctx *fiber.Ctx) error {
 	log.Println("Get Plants")
 	user := getUserQuery(ctx)
 
 	var plants []Plant
 	result := db.Where("user_name = ?", user).Order("alias asc").Find(&plants)
-	if result.Error != nil{
+	if result.Error != nil {
 		log.Println(result.Error)
 	}
-	
+
 	return ctx.Status(fiber.StatusOK).JSON(plants)
 }
 
-func postPlants(ctx *fiber.Ctx) error{
+func postPlants(ctx *fiber.Ctx) error {
 	log.Println("Post Plant")
 
 	var newPlant Plant
-	err := json.Unmarshal( []byte(ctx.FormValue("data")),&newPlant)
+	err := json.Unmarshal([]byte(ctx.FormValue("data")), &newPlant)
 	if err != nil {
 		log.Println(err)
 		return ctx.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	newPlant.ImageName, err =saveImage(ctx, newPlant.UserName)
+	newPlant.ImageName, err = saveImage(ctx, newPlant.UserName)
 	if err != nil {
 		return ctx.SendStatus(fiber.StatusInternalServerError)
 	}
 
 	result := db.Create(&newPlant)
-	if result.Error != nil{
+	if result.Error != nil {
 		log.Println("Plant --> ", newPlant)
 		log.Println(result.Error)
 		return ctx.SendStatus(fiber.StatusInternalServerError)
@@ -119,7 +124,7 @@ func putPlants(ctx *fiber.Ctx) error {
 	log.Println("Put Plant")
 
 	var newPlant Plant
-	err := json.Unmarshal( []byte(ctx.FormValue("data")),&newPlant)
+	err := json.Unmarshal([]byte(ctx.FormValue("data")), &newPlant)
 	if err != nil {
 		log.Println(err)
 		return ctx.SendStatus(fiber.StatusInternalServerError)
@@ -128,7 +133,7 @@ func putPlants(ctx *fiber.Ctx) error {
 	newImageName, err := saveImage(ctx, newPlant.UserName)
 	if err != nil {
 		log.Println(err)
-	}else{
+	} else {
 		newPlant.ImageName = newImageName
 		var oldPlant Plant
 		db.Where("user_name = ? and id = ?", newPlant.UserName, newPlant.ID).First(&oldPlant)
@@ -145,7 +150,27 @@ func putPlants(ctx *fiber.Ctx) error {
 	return ctx.SendStatus(fiber.StatusCreated)
 }
 
-func saveImage(ctx *fiber.Ctx, name string) (string,error){
+func deletePlant(ctx *fiber.Ctx) error {
+	log.Println("Delete Plant")
+
+	var plant Plant
+	err := json.Unmarshal([]byte(ctx.FormValue("data")), &plant)
+	if err != nil {
+		log.Println(err)
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	deleteImage(plant.ImageName)
+	result := db.Delete(&plant)
+	if result.Error != nil {
+		log.Println(result.Error)
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return ctx.SendStatus(fiber.StatusOK)
+}
+
+func saveImage(ctx *fiber.Ctx, name string) (string, error) {
 	log.Println("Save Image")
 
 	file, err := ctx.FormFile("image")
@@ -154,22 +179,22 @@ func saveImage(ctx *fiber.Ctx, name string) (string,error){
 		return "", err
 	}
 
-    uniqueId := uuid.New()
-    filename := name + strings.Replace(uniqueId.String(), "-", "", -1)
+	uniqueId := uuid.New()
+	filename := name + strings.Replace(uniqueId.String(), "-", "", -1)
 	fileExt := strings.Split(file.Filename, ".")[1]
 
-	image := fmt.Sprintf("%s.%s", filename,fileExt)
+	image := fmt.Sprintf("%s.%s", filename, fileExt)
 
 	err = ctx.SaveFile(file, fmt.Sprintf("./images/%s", image))
 	if err != nil {
-        log.Println("image save error --> ", err)
-        return "", err
-    }
+		log.Println("image save error --> ", err)
+		return "", err
+	}
 
 	return image, err
 }
 
-func deleteImage(name string){
+func deleteImage(name string) {
 	log.Println("Delete Image")
 	err := os.Remove(fmt.Sprintf("./images/%s", name))
 	if err != nil {
@@ -179,7 +204,7 @@ func deleteImage(name string){
 
 func getUserQuery(ctx *fiber.Ctx) string {
 	user := ctx.Query("user")
-	if user == ""{
+	if user == "" {
 		log.Println("User Not Set")
 		ctx.SendStatus(fiber.StatusInternalServerError)
 	}
